dd3ae45e4b8c6f0ccd5d50e44f870bcac9a18182
Forrest Huang <huang.4fstudio@gmail.com>
2020-03-27 16:27:36 -0700

    callbacks sans server sending

--- callbacks/test_train.py
+++ callbacks/test_train.py
@@ -1,12 +1,19 @@
 import tensorflow as tf
 from test_model import TestModel
-from callbacks import TestCallback
+from umlaut_callback import UmlautCallback
 
 fashion_mnist = tf.keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
-cb = TestCallback()
-model = TestModel(cb)
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(10))
+
+# Also works with subclass of tf.keras.models.Model
+# model = TestModel() 
+
+cb = UmlautCallback(model)
+
 model.compile(optimizer='adam',
               loss=tf.keras.losses.sparse_categorical_crossentropy,
               metrics=['accuracy'])
---
+++ callbacks/umlaut_callback.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+from tensorflow.keras import backend as K
+import numpy as np
+import types
+import json
+
+class UmlautCallback(tf.keras.callbacks.Callback):
+  def __init__(self, model):
+    self.input_node = tf.Variable(0., validate_shape=False)
+    self.output_node = tf.Variable(0., validate_shape=False)
+    self.register_model(model)
+    K.get_session().run(tf.global_variables_initializer())
+
+  def on_batch_end(self, batch, logs=None):
+    send_obj = {k: float(v) for k, v in logs.items()}
+    send_obj['input_example'] = K.eval(self.input_node)[0].tolist()
+    send_obj['output_example'] = K.eval(self.output_node)[0].tolist()
+
+    self.send_data_to_server(json.dumps(send_obj))
+
+  def register_model(self, model):
+    if not isinstance(model, tf.keras.models.Model):
+      raise NotImplementedError('Umlaut curently doesn\'t support Non-keras models.')
+    
+    current_call = model.call
+    outer_input_node = self.input_node
+    outer_output_node = self.output_node
+    
+    def new_call(self, x, *args, **kwargs):
+        x = tf.assign(outer_input_node, x, validate_shape=False)
+        x = current_call(x, *args, **kwargs)
+        assign_op = tf.assign(outer_output_node, x, validate_shape=False)
+        with tf.control_dependencies([assign_op]):
+            x = tf.identity(x)
+        return x
+
+    model.call = types.MethodType(new_call, model)
+  
+  def send_data_to_server(self, data):
+    pass
