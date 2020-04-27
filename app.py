58310074b3f176f632758b93dc7ae32bdfcd987a
Eldon Schoop <eldonschoop@gmail.com>
2020-04-26 18:22:49 -0700

    api, update db with new metrics

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
 from bson import ObjectId
+from flask import request
 
 from models import db
 
@@ -20,6 +21,36 @@ app = dash.Dash(
     external_stylesheets=external_stylesheets,
     suppress_callback_exceptions=True,  # must have this for dynamic callbacks
 )
+
+# ----------- Flask API routes ---------- #
+
+# get the internal flask object for client facing API
+server = app.server
+
+@server.route('/api/updateSessionPlots/<sess_id>', methods=['POST'])
+def update_session_plots(sess_id):
+    try:
+        sess_id = ObjectId(sess_id)
+    except bson.errors.InvalidId:
+        abort(400)
+    if db.sessions.find_one(sess_id) is None:
+        abort(404)  # session not found
+
+    updates = request.get_json()
+    update_plots = updates.keys()
+    for plot_name in update_plots:  # loss, acc
+        for plot_col in updates[plot_name]:  # train, val
+            update_data = updates[plot_name][plot_col]
+            assert len(list(update_data)) == 2  # [epoch, data]
+            db.plots.update(
+                {'session_id': sess_id, 'name': plot_name},
+                {'$push': {plot_col: update_data}},
+            )
+            print(f'epoch {update_data[0]}: {plot_name}.{plot_col} <-+ {update_data[1]}')
+    return 'done!'
+
+
+# ----------- Helpers ---------- #
 
 def argmax(l):
     return max(range(len(l)), key=lambda i: l[i])
