ff1b11894850961e81551f4c21699574e743c5ee
Eldon Schoop <eldonschoop@gmail.com>
2020-05-15 13:31:43 -0700

    WIP training with tf2

--- test_train.py
+++ test_train.py
@@ -6,12 +6,13 @@ fashion_mnist = tf.keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
 model = tf.keras.Sequential()
+# model.add(tf.keras.layers.Input())
 # model.add(tf.keras.layers.Reshape((28, 28, 1)))
 # model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu'))
 # model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
 # model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
 model.add(tf.keras.layers.Flatten())
-# model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dense(128, activation='relu'))
 model.add(tf.keras.layers.Dense(10))
 
 # Also works with subclass of tf.keras.models.Model
@@ -30,8 +31,8 @@ model.compile(
 )
 
 model.fit(
-    train_images,
-    train_labels,
+    train_images / 256.,
+    train_labels / 256.,
     epochs=10,
     callbacks=[cb],
     validation_split=0.2,  # add validation for val metrics
--- umlaut/callback.py
+++ umlaut/callback.py
@@ -10,45 +10,61 @@ from umlaut.client import UmlautClient
 class UmlautCallback(tf.keras.callbacks.Callback):
     def __init__(self, model, session_name=None, host=None):
 
+        self.model = model
         self.host = host
         if not self.host.startswith('http'):
             self.host = 'http://' + self.host
-        self.input_node = tf.Variable(0., dtype=tf.float32, validate_shape=False)
-        self.output_node = tf.Variable(0., dtype=tf.float32, validate_shape=False)
-        self.register_model(model)
+        self.input_node = tf.Variable(
+            0.,
+            dtype=tf.float32,
+            # shape=self.model.input_shape,
+            shape=tf.TensorShape(None),
+            validate_shape=False,
+            trainable=False,
+        )
+        self.output_node = tf.Variable(
+            0.,
+            dtype=tf.float32,
+            shape=tf.TensorShape(None),
+            # shape=self.model.input_shape,
+            validate_shape=False,
+            trainable=False,
+        )
+        self.register_model(self.model)
         self.umlaut_client = UmlautClient(session_name, host)
 
     def on_epoch_end(self, batch, logs=None):
-        send_obj = {k: float(v) for k, v in logs.items()}
+        # send_obj = {k: float(v) for k, v in logs.items()}
         # send_obj['input_example'] = K.eval(self.input_node)[0].tolist()
         # send_obj['output_example'] = K.eval(self.output_node)[0].tolist()
 
-        self.umlaut_client.send_batch_metrics({
-            'loss': {
-                'train': [batch, float(logs['loss'])],
-                'val': [batch, float(logs['val_loss'])],
-            },
-            'acc': {
-                'train': [batch, float(logs['acc'])],
-                'val': [batch, float(logs['val_acc'])],
-            },
-        })
+        # self.umlaut_client.send_batch_metrics({
+        #     'loss': {
+        #         'train': [batch, float(logs['loss'])],
+        #         'val': [batch, float(logs['val_loss'])],
+        #     },
+        #     'acc': {
+        #         'train': [batch, float(logs['acc'])],
+        #         'val': [batch, float(logs['val_acc'])],
+        #     },
+        # })
         print(logs)
 
     def register_model(self, model):
         if not isinstance(model, tf.keras.models.Model):
             raise NotImplementedError(
-                'Umlaut curently doesn\'t support Non-keras models.')
+                'Umlaut curently doesn\'t support Non-keras models.'
+            )
 
         current_call = model.call
         outer_input_node = self.input_node
         outer_output_node = self.output_node
 
-        def new_call(self, x, *args, **kwargs):
-            x = tf.assign(outer_input_node, tf.cast(x, tf.float32), validate_shape=False)
-            out = current_call(x, *args, **kwargs)
-            assign_op = tf.assign(outer_output_node, tf.cast(out, tf.float32), validate_shape=False)
-            with tf.control_dependencies([assign_op]):
+        def new_call(wrap_instance, x, *args, **kwargs):  # self here is the model, not the callback
+            outer_assign = tf.compat.v1.assign(outer_input_node, tf.cast(x, tf.float32), validate_shape=False)
+            out = current_call(outer_assign, *args, **kwargs)
+            output_assign = tf.compat.v1.assign(outer_output_node, tf.cast(out, tf.float32), validate_shape=False)
+            with tf.control_dependencies([output_assign]):
                 out = tf.identity(out)
             return out
 
