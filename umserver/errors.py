22de7d47cf2233e4edda10a7058b742c88a9ab36
Eldon Schoop <eldonschoop@gmail.com>
2020-06-30 19:15:26 +0000

    Update error message copy + so + docs links

--- umserver/errors.py
+++ umserver/errors.py
@@ -10,8 +10,10 @@ def get_error_color(error_idx):
 
 class BaseErrorMessage:
     title = 'Base Error Message'
-    subtitle = 'Override me.'
-    _md_solution = ['Base error message. Override me.']
+    subtitle = 'Base subtitle message'
+    _md_solution = None
+    _so_query = None
+    _docs_url = None
 
     @property
     def description(self):
@@ -23,13 +25,25 @@ class BaseErrorMessage:
     def is_static(self):
         return self.epochs is None
 
+    def get_context_subtitle(self):
+        if self.remarks:
+            return self.subtitle.format(remarks)
+        elif '{' in self.subtitle:
+            return self.subtitle.format('')
+        return self.subtitle
+
     def get_annotations(self):
         return [(e - 1, e) for e in self.epochs]
 
     def render(self, error_index):
+        '''Formats an error message as a Dash html component.
+        
+        This method assigns the id "types" of 'error-msg' and
+        'error-msg-btn-annotate' which are used by callbacks.
+        '''
         error_fmt = [
             html.H3(self.title),
-            dcc.Markdown(self.subtitle),
+            dcc.Markdown(self.get_context_subtitle()),
             html.H4('Solution'),
             dcc.Markdown(self.description),
         ]
@@ -46,36 +60,36 @@ class BaseErrorMessage:
         ))
         
         error_fmt.append(html.Br())
-        error_fmt.append(html.A(
-            [
-                html.Img(
-                    src='https://cdn.sstatic.net/Sites/stackoverflow/company/Img/logos/so/so-icon.svg',
-                    height='15px',
-                    style={'paddingRight': '5px', 'margin-bottom': '-3px'},
+        if self._so_query:
+            error_fmt.append(html.A(
+                [
+                    html.Img(
+                        src='https://cdn.sstatic.net/Sites/stackoverflow/company/Img/logos/so/so-icon.svg',
+                        height='15px',
+                        style={'paddingRight': '5px', 'margin-bottom': '-3px'},
+                    ),
+                        'Search Stack Overflow',
+                ],
+                href='https://stackoverflow.com/search?{}'.format(
+                    parse.urlencode(self._so_query),
                 ),
-                    'Search Stack Overflow',
-            ],
-            href='https://stackoverflow.com/search?{}'.format(
-                parse.urlencode({'q': '[keras] overfitting'}),
-            ),
-            target='_blank',
-            style={'paddingRight': '1rem'},
-        ))
-        error_fmt.append(html.A(
-            [
-                html.Img(
-                    src='https://upload.wikimedia.org/wikipedia/commons/2/2d/Tensorflow_logo.svg',
-                    height='15px',
-                    style={'paddingRight': '5px', 'margin-bottom': '-3px'},
-                ),
-                    'Search Docs',
-            ],
-            href='https://www.tensorflow.org/s/results?{}'.format(
-                parse.urlencode({'q': 'keras losses'}),
-            ),
-            target='_blank',
-        ))
-        error_fmt.append(html.Hr())
+                target='_blank',
+                style={'paddingRight': '1rem'},
+            ))
+        if self._docs_url:
+            error_fmt.append(html.A(
+                [
+                    html.Img(
+                        src='https://upload.wikimedia.org/wikipedia/commons/2/2d/Tensorflow_logo.svg',
+                        height='15px',
+                        style={'paddingRight': '5px', 'margin-bottom': '-3px'},
+                    ),
+                        'Search Docs',
+                ],
+                href=self._docs_url,
+                target='_blank',
+            ))
+            error_fmt.append(html.Hr())
 
         return html.Div(
             error_fmt,
@@ -83,8 +97,9 @@ class BaseErrorMessage:
             style={'cursor': 'pointer', 'display': 'inline-block'},
         )
 
-    def __init__(self, epochs, *args, **kwargs):
+    def __init__(self, epochs, remarks=None, *args, **kwargs):
         self.epochs = epochs
+        self.remarks = remarks
         if type(self.epochs) is not list:
             self.epochs = [epochs]
 
@@ -96,22 +111,27 @@ class BaseErrorMessage:
 
 
 class InputNotNormalizedError(BaseErrorMessage):
-    title = 'Input Data Exceeds Typical Limits'
-    subtitle = 'Your input data does not look normalized.{}'
+    title = 'Input data exceeds typical limits'
+    subtitle = 'Your input data does not look normalized. {}'
+    _so_query = {'q': '[keras] closed:yes normalization'}
+    _docs_url = 'https://www.tensorflow.org/tutorials/keras/classification#preprocess_the_data'
     _md_solution = [
-        'You should normalize the input data where its values fall between the typical ranges of 0 to 1 or -1 to 1, before passing them into the model. E.g., for image data (pixels ranging from 0-255), a typical way to normalize the pixel values to the range of -1 to 1 is:',
-        '`your_input_images = (your_input_images / 127.0) - 1`',
+        'You should normalize the input data so its values fall between the typical ranges of -1 to 1 before passing them into the model',
+        'For image data, (pixels ranging from 0-255), a typical way to normalize the pixel values to the range of -1 to 1 is',
+        '`training_images = (training_images / 128.0) - 1`',
     ]
-    def __init__(self, epochs, remarks=''):
-        super().__init__(epochs)
-        self.subtitle = InputNotNormalizedError.subtitle.format(
-            ' (' + remarks + ')',
-        )
+    # def __init__(self, epochs, remarks):
+    #     super().__init__(epochs, remarks)
+    #     if remarks:
+    #         remarks = '(' + remarks + ')'
+    #     self.subtitle = InputNotNormalizedError.subtitle.format(remarks)
     
 
 class InputNotFloatingError(BaseErrorMessage):
     title = 'Input is not a Float type'
     subtitle = 'Your input is not a floating type.'
+    _so_query = {'q': '[keras] closed:yes float'}
+    _docs_url = 'https://www.tensorflow.org/tutorials/keras/classification#preprocess_the_data'
     _md_description = [
         'Your input should be a floating point type (supporting decimals), rather than an integer type. This allows gradients to propogate properly to your neural net\'s weights.',
         'You can either implicitly change the type of your input (e.g., by dividing by a float): `X_train = X_train / 1.0`, or by setting the `dtype` of your input to something such as `tf.float32`.',
@@ -121,6 +141,7 @@ class InputNotFloatingError(BaseErrorMessage):
 class NaNInLossError(BaseErrorMessage):
     title = 'NaN (Not a number) in loss'
     subtitle = 'The loss value of your model has gone to NaN (could indicate infinity). This could be caused by a learning rate that is too high.'
+    _so_query = {'q': '[keras] nan loss'}
     _md_solution = [
         'You can set your learning rate when you create your optimizer object. Typical learning rates for the Adam optimizer are between 0.00001 and 0.01. For example:',
         '`model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001))`',
@@ -130,6 +151,8 @@ class NaNInLossError(BaseErrorMessage):
 class NoSoftmaxActivationError(BaseErrorMessage):
     title = 'Loss function expects normalized input'
     subtitle = 'The loss function of your model expects a probability distribution as input (i.e., the likelihood for all the classes sums to 1), but your model is producing un-normalized outputs, called "logits". Logits can be normalized to a probability distribution with a [softmax](https://www.tensorflow.org/api_docs/python/tf/keras/layers/Softmax) layer.'
+    _so_query = {'q': '[keras] is:closed from_logits'}
+    _docs_url = 'https://www.tensorflow.org/api_docs/python/tf/keras/losses'
     _md_solution = [
         'Many Keras loss function [classes](https://www.tensorflow.org/api_docs/python/tf/keras/losses) can automatically compute softmax for you by passing in a `from_logits` flag:',
         '`tf.keras.losses.<your loss function here>(from_logits=True)`',
@@ -147,8 +170,10 @@ class NoSoftmaxActivationError(BaseErrorMessage):
 class OverfittingError(BaseErrorMessage):
     title = 'Possible Overfitting'
     subtitle = 'The validation loss is increasing while training loss is stuck or decreasing. This could indicate overfitting.'
+    _so_query = {'q': '[keras] is:closed regularization'}
+    _docs_url = 'https://www.tensorflow.org/api_docs/python/tf/keras/regularizers/Regularizer'
     _md_solution = [
-        'Try reducing the power of your model or adding regularlization. You can reduce the power of your model by decreasing the `units` or `filters` parameters of `Dense` or `Conv2D` layers.',
+        'Try reducing the power of your model or adding regularization. You can reduce the power of your model by decreasing the `units` or `filters` parameters of `Dense` or `Conv2D` layers.',
         'Regularization penalizes weights which are high in magnitude. You can try adding L2 or L1 regularization by using a [regularizer](https://www.tensorflow.org/api_docs/python/tf/keras/regularizers).'
     ]
 
